b1b2c9e63b9d1cd14997635c83f9797244a3d514
Peter Gillis <petergillis@Peters-MacBook-Air.local>
2017-11-08 11:44:52 -0400

    pong

---
+++ Form1.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Pong
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.Computer = new System.Windows.Forms.PictureBox();
+            this.ComputerScore = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.PlayerScore = new System.Windows.Forms.Label();
+            this.Player = new System.Windows.Forms.PictureBox();
+            this.Ball = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Computer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Computer
+            // 
+            this.Computer.BackColor = System.Drawing.Color.Red;
+            this.Computer.Location = new System.Drawing.Point(12, 147);
+            this.Computer.Name = "Computer";
+            this.Computer.Size = new System.Drawing.Size(16, 128);
+            this.Computer.TabIndex = 0;
+            this.Computer.TabStop = false;
+            // 
+            // ComputerScore
+            // 
+            this.ComputerScore.AutoSize = true;
+            this.ComputerScore.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ComputerScore.Location = new System.Drawing.Point(51, 42);
+            this.ComputerScore.Name = "ComputerScore";
+            this.ComputerScore.Size = new System.Drawing.Size(30, 33);
+            this.ComputerScore.TabIndex = 1;
+            this.ComputerScore.Text = "0";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // PlayerScore
+            // 
+            this.PlayerScore.AutoSize = true;
+            this.PlayerScore.Font = new System.Drawing.Font("Comic Sans MS", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.PlayerScore.Location = new System.Drawing.Point(518, 42);
+            this.PlayerScore.Name = "PlayerScore";
+            this.PlayerScore.Size = new System.Drawing.Size(30, 33);
+            this.PlayerScore.TabIndex = 2;
+            this.PlayerScore.Text = "0";
+            // 
+            // Player
+            // 
+            this.Player.BackColor = System.Drawing.Color.Blue;
+            this.Player.Location = new System.Drawing.Point(596, 147);
+            this.Player.Name = "Player";
+            this.Player.Size = new System.Drawing.Size(16, 128);
+            this.Player.TabIndex = 3;
+            this.Player.TabStop = false;
+            // 
+            // Ball
+            // 
+            this.Ball.BackColor = System.Drawing.Color.Transparent;
+            this.Ball.ErrorImage = global::WindowsFormsApplication2.Properties.Resources.ball4;
+            this.Ball.Image = global::WindowsFormsApplication2.Properties.Resources.ball4;
+            this.Ball.InitialImage = global::WindowsFormsApplication2.Properties.Resources.ball4;
+            this.Ball.Location = new System.Drawing.Point(304, 157);
+            this.Ball.Name = "Ball";
+            this.Ball.Size = new System.Drawing.Size(20, 20);
+            this.Ball.TabIndex = 4;
+            this.Ball.TabStop = false;
+            // 
+            // Pong
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(624, 442);
+            this.Controls.Add(this.Ball);
+            this.Controls.Add(this.Player);
+            this.Controls.Add(this.PlayerScore);
+            this.Controls.Add(this.ComputerScore);
+            this.Controls.Add(this.Computer);
+            this.Name = "Pong";
+            this.Text = "Pong";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Computer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Player)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ball)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Computer;
+        private System.Windows.Forms.Label ComputerScore;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label PlayerScore;
+        private System.Windows.Forms.PictureBox Player;
+        private System.Windows.Forms.PictureBox Ball;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Pong : Form
+    {
+        int compScore;
+        int plrScore;
+        double speed;
+        double xVel;
+        double yVel;
+        
+        public Pong()
+        {
+            InitializeComponent();
+            compScore = 0;
+            plrScore = 0;
+            speed = 40;
+            Random random = new Random();
+            xVel = Math.Cos(random.Next(5, 10)) * speed;
+            yVel = Math.Sin(random.Next(5, 10)) * speed; 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            Ball.Location = new Point(Convert.ToInt32(Ball.Location.X + xVel), Convert.ToInt32(Ball.Location.Y + yVel));
+            //move computer paddle
+            if (Ball.Location.Y > 5 && Ball.Location.Y < this.Height - 40 - Player.Height)
+            {
+                Computer.Location = new Point(Computer.Location.X, Ball.Location.Y);
+            }
+            //check top wall
+            if (Ball.Location.Y < 0)
+            {
+                Ball.Location = new Point(Ball.Location.X, 0);
+                yVel = -yVel;
+            }
+            //check bottom wall
+            if (Ball.Location.Y > this.Height - Ball.Size.Height - 45)
+            {
+                Ball.Location = new Point(Ball.Location.X, this.Height - Ball.Size.Height - 45);
+                yVel = -yVel;
+            }
+            //check left wall
+            if (Ball.Location.X < 0)
+            {
+                plrScore += 1;
+                Ball.Location = new Point(this.Size.Width / 2, this.Size.Height / 2);
+                PlayerScore.Text = Convert.ToString(plrScore);
+            }
+            //check right wall
+            if (Ball.Location.X > this.Width - Ball.Size.Width - Player.Width)
+            {
+                compScore += 1;
+                Ball.Location = new Point(this.Size.Width / 2, this.Size.Height / 2);
+                ComputerScore.Text = Convert.ToString(compScore);
+            }
+            //check player paddle
+            if (Ball.Bounds.IntersectsWith(Player.Bounds))
+            {
+                Ball.Location = new Point(Player.Location.X - Ball.Size.Width, Ball.Location.Y);
+                xVel = -xVel;
+            }
+            //check computer paddle
+            if (Ball.Bounds.IntersectsWith(Computer.Bounds))
+            {
+                Ball.Location = new Point(Computer.Location.X + Computer.Size.Width + 1, Ball.Location.Y);
+                xVel = -xVel;
+            }
+
+            if (compScore == 15 || plrScore == 15)
+            {
+                PauseGame();
+                PlaySound();
+                DialogResult result = MessageBox.Show("Play again?", "Game over.", MessageBoxButtons.YesNo);
+                if (result == DialogResult.Yes)
+                {
+                    ResetScore();
+                    PauseGame();
+                }
+                else if (result == DialogResult.No)
+                {
+                    this.Close();
+                }
+            }
+        }
+        private void ResetScore()
+        {
+            compScore = 0;
+            ComputerScore.Text = Convert.ToString(compScore);
+            plrScore = 0;
+            PlayerScore.Text = Convert.ToString(plrScore);
+        }
+        protected override void OnMouseMove(MouseEventArgs e)
+        {
+            if (e.Y > 5 & e.Y < this.Height - 40 - Player.Height)
+            {
+                Player.Location = new Point(Player.Location.X, e.Y);
+            }
+        }
+
+        private void PlaySound()
+        {
+            SoundPlayer sp = new SoundPlayer(Properties.Resources.game);
+            sp.Play();
+        }
+
+        private void PauseGame()
+        {
+            if (timer1.Enabled == true)
+            {
+                timer1.Enabled = false;
+            }
+            else if (timer1.Enabled == false)
+            {
+                timer1.Enabled = true;
+            }
+        }
+
+    }
+}
